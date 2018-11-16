adcdeadc9b89c8bbccee0afaa7e912b16498c394
keep-looking <8t9587+16br9e37xeuko@sharklasers.com>
2018-11-17 08:48:02 +0900

    debug

--- peer.go
+++ peer.go
@@ -464,6 +464,8 @@ func IndexOfFunc(l *Ledger, acquireLedger func(LedgerID) (*Ledger, error)) func(
 		var ll *Ledger
 		for ll = l; ll.Seq != s && ll.Seq > 0; {
 			log.Println(ll, ll.Seq)
+			id := ll.ID()
+			log.Println(hex.EncodeToString(id[:]), hex.EncodeToString(ll.ParentID[:]))
 			var err error
 			ll, err = acquireLedger(ll.ParentID)
 			if err != nil {
